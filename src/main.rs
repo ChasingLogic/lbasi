d1f502e1bb92ce59c7813d7444e3e3e8bc70f1d2
Mathew Robinson <mathew.robinson@kroger.com>
2016-05-06 15:04:48 -0400

    Running a little different test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ extern crate lbasi;
 use lbasi::*;
 
 fn main() {
-    println!("{}", interpreter::Token::new("6".to_string()));
-    println!("{}", interpreter::Token::new("+".to_string()));
-    println!("{}", interpreter::Token::new("".to_string()))
+    println!("Starting");
+    let answer = interpreter::run("33 + 25 + 10".to_string())
+        .expect("Got an error running interpreter");
+    println!("Stopping");
 }
