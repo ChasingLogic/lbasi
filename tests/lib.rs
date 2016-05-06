9b2743353f723589213787ac1f97bda3d2b29350
Mathew Robinson <mathew.robinson@kroger.com>
2016-05-06 15:05:01 -0400

    Added unit tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,7 +3,31 @@ use lbasi::*;
 
 #[test]
 fn test_token_creation() {
-    assert!(interpreter::Token{ kind: interpreter::TokenType::Integer, value: "6".to_string() } == interpreter::Token::new("6".to_string()));
-    assert!(interpreter::Token{ kind: interpreter::TokenType::Plus, value: "+".to_string() }    == interpreter::Token::new("+".to_string()));
-    assert!(interpreter::Token{ kind: interpreter::TokenType::EOF, value: "".to_string() }      == interpreter::Token::new("".to_string()));
+    assert!(interpreter::Token{ kind: interpreter::TokenType::Integer, value: '6' } == interpreter::Token::new('6'));
+    assert!(interpreter::Token{ kind: interpreter::TokenType::Plus, value: '+' }    == interpreter::Token::new('+'));
+    assert!(interpreter::Token{ kind: interpreter::TokenType::EOF, value: 'E' }      == interpreter::Token::new('E'));
+}
+
+#[test]
+fn test_simple_addition() {
+    let answer = interpreter::run("3 + 5".to_string())
+        .expect("Got an error running interpreter");
+    
+    assert!(answer == 8, "answer = {}", answer)
+}
+
+#[test]
+fn test_complex_addition() {
+    let answer = interpreter::run("3 + 5 + 6 + 9".to_string())
+        .expect("Got an error running interpreter");
+    
+    assert!(answer == 23, "answer = {}", answer)
+}
+
+#[test]
+fn test_complex_numbers() {
+    let answer = interpreter::run("33 + 25 + 10".to_string())
+        .expect("Got an error running interpreter");
+    
+    assert!(answer == 68, "answer = {}", answer)
 }
