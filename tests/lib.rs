22cf654a4914b907687967c7e623046ce22f6af9
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-02-06 18:23:44 -0500

    change tests to match new syntax

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,26 +3,32 @@ use lbasi::*;
 
 #[test]
 fn test_token_creation() {
-    assert!(interpreter::Token{ kind: interpreter::TokenType::Integer, value: '6' } == interpreter::Token::new('6'));
-    assert!(interpreter::Token{ kind: interpreter::TokenType::Plus, value: '+' }    == interpreter::Token::new('+'));
-    assert!(interpreter::Token{ kind: interpreter::TokenType::Subtract, value: '-' }    == interpreter::Token::new('-'));
+    assert!(interpreter::Token{ kind: interpreter::TokenType::Integer, value: "6".to_string()} == interpreter::Token::new('6'));
+    assert!(interpreter::Token{ kind: interpreter::TokenType::Add, value: "+".to_string()}    == interpreter::Token::new('+'));
+    assert!(interpreter::Token{ kind: interpreter::TokenType::Subtract, value: "-".to_string()}    == interpreter::Token::new('-'));
+    assert!(interpreter::Token{ kind: interpreter::TokenType::Multiply, value: "*".to_string()}    == interpreter::Token::new('*'));
+    assert!(interpreter::Token{ kind: interpreter::TokenType::Divide, value: "/".to_string()}    == interpreter::Token::new('/'));
 }
 
 #[test]
 fn test_interpreter() {
-    interpreter_test("5 - 3".to_string(), 2);
-    interpreter_test("4 - 2 - 4".to_string(), -2);
-    interpreter_test("10 - 2 - 4".to_string(), 4);
-    interpreter_test("5 + 3".to_string(), 8);
-    interpreter_test("3 + 5 + 6 + 9".to_string(), 23);
-    interpreter_test("33 + 25 + 10".to_string(), 68);
-    interpreter_test("22 - 11 + 10 - 21".to_string(), 0);
-    interpreter_test("3 + 4 - 2".to_string(), 5);
+    interpreter_test("5 - 3", 2);
+    interpreter_test("4 - 2 - 4", -2);
+    interpreter_test("10 - 2 - 4", 4);
+    interpreter_test("5 + 3", 8);
+    interpreter_test("3 + 5 + 6 + 9", 23);
+    interpreter_test("33 + 25 + 10", 68);
+    interpreter_test("22 - 11 + 10 - 21", 0);
+    interpreter_test("3 + 4 - 2", 5);
+    interpreter_test("2 * 3", 6);
+    interpreter_test("1 * 3", 3);
+    interpreter_test("4 / 2", 2);
+    interpreter_test("20 / 2", 10);
 }
 
-fn interpreter_test(expr: String, expected: i32) {
-    let answer = interpreter::run(expr.clone())
+fn interpreter_test(expr: &str, expected: i32) {
+    let answer = interpreter::run(expr.to_string().clone())
         .expect("Got an error running interpreter");
-    
+
     assert!(answer == expected, "expr = {} answer = {}", expr, answer)
 }
